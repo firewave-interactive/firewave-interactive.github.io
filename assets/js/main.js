3f9a14a0255579ef0039b63ea7f57630ed41a759
gabriel-aplok <97059054+gabriel-aplok@users.noreply.github.com>
2023-02-05 11:17:08 -0300

    Added theme system.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
-footer = function() {
+footer = () => {
 	document.querySelector(".date-year").innerText = new Date().getFullYear();
 }
 
 footer();
 
-cookieConsent = function() {
-	document.addEventListener("DOMContentLoaded", function() {
+cookieConsent = () => {
+	document.addEventListener("DOMContentLoaded", () => {
 		const cookieConsent = document.querySelector(".cookie-consent");
 		const cookieConsentButton = document.querySelector(".cookie-consent-button");
 
@@ -13,11 +13,11 @@ cookieConsent = function() {
 			cookieConsent.style.display = "block";
 		}
 
-		cookieConsent.addEventListener("click", function(event) {
+		cookieConsent.addEventListener("click", (event) => {
 			event.preventDefault();
 		});
 
-		cookieConsentButton.addEventListener("click", function() {
+		cookieConsentButton.addEventListener("click", () => {
 			localStorage.setItem("cookieConsent", true);
 			cookieConsent.style.display = "none";
 		});
@@ -25,3 +25,35 @@ cookieConsent = function() {
 }
 
 cookieConsent();
+
+theme = () => {
+	const toggleTheme = document.querySelector(".toggle-theme");
+	const moon = toggleTheme.firstElementChild;
+	const sun = toggleTheme.lastElementChild;
+
+	if (localStorage.getItem("theme") === "light") {
+		document.body.classList.add("light-theme");
+		moon.style.display = "none";
+		sun.style.display = "inline-block";
+	} else {
+		document.body.classList.remove("light-theme");
+		moon.style.display = "inline-block";
+		sun.style.display = "none";
+	}
+
+	toggleTheme.addEventListener("click", () => {
+		document.body.classList.toggle("light-theme");
+
+		if (document.body.classList.contains("light-theme")) {
+			localStorage.setItem("theme", "light");
+			moon.style.display = "none";
+			sun.style.display = "inline-block";
+		} else {
+			localStorage.setItem("theme", "dark");
+			moon.style.display = "inline-block";
+			sun.style.display = "none";
+		}
+	});
+}
+
+theme();
